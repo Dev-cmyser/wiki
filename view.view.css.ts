ffa569882c8d5d33d82e3845d0f49f98e7c2e3ff
novgorodcev.k <kirrallt@mail.ru>
2025-03-10 17:36:37 +0300

    wiki

---
+++ view.view.css.ts
@@ -0,0 +1,9 @@
+namespace $ {
+	$mol_style_define ( $first_wiki , {
+		color: "red",
+		
+		Body:{
+			padding: $mol_gap.blur
+		}
+	})
+}
---
+++ wiki.view.ts
@@ -0,0 +1,8 @@
+namespace $.$$ {
+	export class $first_wiki extends $.$first_wiki {
+		text( next?: string ): string {
+			return this.$.$mol_state_local.value ("text", next)  ?? ''
+			
+		}
+	}
+}
