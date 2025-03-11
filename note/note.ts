70882057cc67daa18fb4bc1fed0b4fe7dbeaf07f
novgorodcev.k <kirrallt@mail.ru>
2025-03-11 18:46:24 +0300

    fix

--- note/note.ts
+++ note/note.ts
@@ -1,8 +1,5 @@
 namespace $ {
-	export class $wiki_note extends $hyoo_crus_entity.with({
+	export class $cmyser_wiki_note extends $hyoo_crus_entity.with({
 		Body: $hyoo_crus_dom,
-	}) {
-
-	}
-
+	}) {}
 }
--- view.view.css.ts
+++ view.view.css.ts
@@ -1,8 +1,17 @@
 namespace $ {
-	$mol_style_define ( $wiki , {
-		color: "green",
-		Body:{
-			padding: $mol_gap.blur
-		}
+	const { per } = $mol_style_unit
+
+	$mol_style_define($cmyser_wiki, {})
+	$mol_style_define($mol_book2, {
+		flex: 'auto',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'stretch',
+		justifyContent: 'stretch',
+		overflow: 'hidden',
+		backgroundColor: '',
+		'--lightness': per(0.45),
+		'--chroma': per(0.1),
+		'--hue': per(186),
 	})
 }
