116d483ec978cb18feb9060d91b3b9f6019b63cb
novgorodcev.k <kirrallt@mail.ru>
2025-03-29 21:28:17 +0300

    yes sience

--- pixelbattle.view.ts
+++ pixelbattle.view.ts
@@ -1,3 +1,12 @@
 namespace $.$$ {
-    export class $pixelbattle extends $.$pixelbattle {}
+	export class $pixelbattle extends $.$pixelbattle {
+		@$mol_mem
+		theme_mode(next?: string) {
+			console.log(next)
+			if (next) {
+				document.documentElement.setAttribute('mol_theme', next)
+			}
+			return document.documentElement.getAttribute('mol_theme') || 'light'
+		}
+	}
 }
