0216bfc67a6f86726164cd6cefff822e1e7b9f49
novgorodcev.k <kirrallt@mail.ru>
2025-03-29 22:19:10 +0300

    yes sience

--- workspace/board/board.view.ts
+++ workspace/board/board.view.ts
@@ -2,18 +2,20 @@ namespace $.$$ {
 	export class $pixelbattle_workspace_board extends $.$pixelbattle_workspace_board {
 		@$mol_mem
 		pixels(): readonly $pixelbattle_workspace_board_pixel[] {
-			const size = 500
+			const size = 100
 			const items: $pixelbattle_workspace_board_pixel[] = []
 			for (let y = 0; y < size; y++) {
 				for (let x = 0; x < size; x++) {
-					items.push(
-						$pixelbattle_workspace_board_pixel.make({
-							id: () => `${x}_${y}`,
-						}),
-					)
+					items.push(this.Pixel(x + '_' + y))
 				}
 			}
 			return items
+		}
+
+		Pixel(id: string) {
+			return this.$.$pixelbattle_workspace_board_pixel.make({
+				id: $mol_const(id),
+			})
 		}
 	}
 }
