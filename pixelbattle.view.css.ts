26188d2a8957e1a5dc9569c4bffe2fe525a31247
novgorodcev.k <kirrallt@mail.ru>
2025-03-30 22:34:00 +0300

    yes

--- pixelbattle.view.css.ts
+++ pixelbattle.view.css.ts
@@ -1,17 +1,8 @@
 namespace $ {
 	const { per } = $mol_style_unit
 
-	$mol_style_define($pixelbattle, {})
-	$mol_style_define($mol_book2, {
-		flex: 'auto',
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'stretch',
-		justifyContent: 'stretch',
-		overflow: 'hidden',
-		backgroundColor: '',
-		'--lightness': 0.45,
-		'--chroma': 0.1,
-		'--hue': 186,
+	$mol_style_define($mol_theme_auto, {
+		'--mol_theme_hue': 660,
+		'--mol_theme_hue_spread': 60,
 	})
 }
--- pixelbattle.view.ts
+++ pixelbattle.view.ts
@@ -1,19 +1,3 @@
 namespace $.$$ {
-	export class $pixelbattle extends $.$pixelbattle {
-		@$mol_mem
-		theme_mode(next?: string) {
-			console.log(next)
-			if (next) {
-				document.documentElement.setAttribute('mol_theme', next)
-			}
-			return document.documentElement.getAttribute('mol_theme') || 'light'
-		}
-		hue_deg() {
-			return 660 + 'deg'
-		}
-
-		hue_spread_deg() {
-			return 60 + 'deg'
-		}
-	}
+	export class $pixelbattle extends $.$pixelbattle {}
 }
